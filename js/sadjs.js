0ca69ea9062d615b1f7668d5f8e0b9ce860618c4
Yuqi Zhang <xtrangev@outlook.com>
2020-05-26 17:08:37 +0800

    wow

--- js/sadjs.js
+++ js/sadjs.js
@@ -1,19 +1,5 @@
 function sad()
 {
-	var num = new XMLHttpRequest();
-	num.open("get","https://github.com/XtrangeV/xtrangev.github.io/num.txt",true);
-	num.send();
-	num.onreadystatechange = function()
-	{
-		if(xhr.readyState == 4 && xhr.status == 200)
-		{
-			console.log(xhr);
-			console.log(xhr.responseText);
-		} 
-		else if(xhr.status == 404)
-		{
-			console.log(xhr.status);
-		}
-	}
+	var num = document.getElementById("number");
+	num.innerHTML++;
 }
-sad();
