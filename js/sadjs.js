0e24622d8900913e70360c48252770250cb41214
Yuqi Zhang <xtrangev@outlook.com>
2020-06-06 12:25:46 +0800

    ajaxmaybe

--- js/sadjs.js
+++ js/sadjs.js
@@ -7,3 +7,22 @@ function sad()
 		alert("wow nice guy");
 	}
 }
+function ajaxtest()
+{
+	var xmlhttp;
+	
+	if(window.XMLHttpRequest)
+	{
+		xmlhttp = new XMLHttpRequest();
+	}
+	xmlhttp.onreadystatechange = function()
+	{
+		if(xmlhttp.readyState == 4&& xmlhttp.status == 200)
+		{
+			document.getElementById("numdiv").innerHTML = xmlhttp.responseText;
+		}
+	}
+	xmlhttp.open("GET","192.168.2.13/1.txt",true);
+	xmlhttp.send();
+	
+}
